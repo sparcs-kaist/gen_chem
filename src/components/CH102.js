1a2832b225f4c2b73827de9d257dbf1072e851be
jaejun1 <ltq3021@gmail.com>
2017-11-08 20:36:00 +0900

    file download completed

--- src/components/CH102.js
+++ src/components/CH102.js
@@ -6,6 +6,7 @@ import Notice from './Notice';
 import UsefulLinks from "./UsefulLinks";
 import TAContact from "./TAContact";
 import Evaluation from './Evaluation';
+import Download from './Download'
 
 export default class CH102 extends Component {
   constructor(props) {
@@ -40,7 +41,7 @@ export default class CH102 extends Component {
         <div style={{paddingTop: 64}}>
           <Switch>
             <Route path='/ch102/notice' render={() => <Notice onPress={this.dropUp}/>} />
-            <Route path='/ch102/schedule' render={() => <div style={{height: 1080}}>Hi2</div>} />
+            <Route path='/ch102/schedule' render={() => <Download />} />
             <Route path='/ch102/evaluation' render={() => <Evaluation />} />
             <Route path='/ch102/safety' render={() => <div style={{height: 1080}}>Hi4</div>} />
             <Route path='/ch102/links' render={() => <div style={{minHeight: '100vh'}}><UsefulLinks /></div>} />
