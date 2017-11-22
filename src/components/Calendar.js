e4f30a213a2eb56d21e5758623212751974abbba
hyeongjune <hyeongjune@hyeongjuneui-MacBook-Pro.local>
2017-11-22 21:02:32 +0900

    new Calendar component

---
+++ src/components/Calendar.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import '../grid.css';
+
+function getPrevMonth(month) {
+    if (month == 1)
+        return 12;
+
+    return month-1;
+}
+
+function getFirstDay(year, month) {
+    var days = 1;
+
+    days += (year-1900)*365;
+    days += parseInt((year-1900)/4);
+    if (month < 3 && year%4 == 0) days -= 1;
+
+    for (var i=1;i<month;i++) {
+        days += getDaysofMonth(year,i);
+    }
+
+    return days%7;
+}
+
+function getDaysofMonth(year, month) {
+    if (month == 2) {
+        if (year%4 == 0) return 29;
+        else return 28;
+    }
+
+    if (month < 8) {
+        if (month%2 == 0) return 30;
+        else return 31;
+    }
+
+    if (month%2 == 0) return 31;
+    return 30;
+}
+
+function getDayList(year, month) {
+    var days = [];
+    var firstDay = getFirstDay(year, month);
+    var dayNum = getDaysofMonth(year, month);
+    var prevMonth = getPrevMonth(year, month);
+
+    var i;
+    var cnt = 0;
+    for (i=0;i<firstDay;i++) {
+        var prevDay = getDaysofMonth(year,prevMonth)-firstDay+i+1;
+        days.push({
+            Day : prevDay.toString(),
+            info : "Nothing",
+            stat : "prev"
+        });
+        cnt++;
+    }
+
+    for (i=1;i<=dayNum;i++) {
+        days.push({
+            Day : i.toString(),
+            info : "Nothing",
+            stat : "curr"
+        })
+        cnt++;
+    }
+
+    var leftDay = 42 - cnt  ;
+    for (i=1;i<=leftDay;i++) {
+        days.push({
+            Day : i.toString(),
+            info : "Nothing",
+            stat : "next"
+        })
+    }
+
+    return days;
+}
+
+const day  = {
+    width : '14%',
+    height : "70px",
+    /*backgroundColor: "lightgray",*/
+    textAlign : "center",
+    marginTop : '1px',
+    fontSize : "20pt",
+    float : 'left',
+    lineHeight : "70px"
+}
+
+const notCurrday  = {
+    width : '14%',
+    height : "70px",
+    /*backgroundColor: "lightgray",*/
+    marginTop : '1px',
+    textAlign : "center",
+    fontSize : "20pt",
+    float : 'left',
+    lineHeight : "70px",
+    color : "#C2C2C2"
+}
+
+export default class Calendar extends Component {
+    render () {
+        var dayList = getDayList(this.props.Year, this.props.Month);
+        var year = this.props.Year;
+
+        return (
+            <div>
+            {
+            dayList.map((item)=> {
+                    return (
+                        <div>
+                            {item.stat=="curr"?
+                                <div style={day} onClick={() => {this.props.setPost(item.info)}}>
+                                    {item.Day}
+                                </div> :
+                                <div style={notCurrday}>
+                                    {item.Day}
+                                </div>
+                            }
+                        </div>
+                    );
+                }
+            )
+            }
+            </div>
+        );
+    }
+}
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,16 +1,9 @@
 import React, { Component } from 'react';
-import {Link, Route} from 'react-router-dom';
 import '../grid.css';
-import Info from './Info.js';
 import './infostyle.css';
-import left from './img/left.png'
-import right from './img/right.png'
 
-
-var today = new Date();
-var todayYear = today.getFullYear();
-var todayMonth = today.getMonth()+1;
-var todayDate = today.getDate();
+import Calendar from './Calendar';
+import CalendarHead from "./CalendarHead";
 
 function getNextMonth(month) {
     if (month == 12)
@@ -24,97 +17,6 @@ function getPrevMonth(month) {
         return 12;
 
     return month-1;
-}
-
-function getDaysofMonth(year, month) {
-    if (month == 2) {
-        if (year%4 == 0) return 29;
-        else return 28;
-    }
-
-    if (month < 8) {
-        if (month%2 == 0) return 30;
-        else return 31;
-    }
-
-    if (month%2 == 0) return 31;
-    return 30;
-}
-
-function getFirstDay(year, month) {
-    var days = 1;
-
-    days += (year-1900)*365;
-    days += parseInt((year-1900)/4);
-    if (month < 3 && year%4 == 0) days -= 1;
-
-    for (var i=1;i<month;i++) {
-        days += getDaysofMonth(year,i);
-    }
-
-    return days%7;
-}
-
-function getDayList(year, month) {
-    var days = [];
-    var firstDay = getFirstDay(year, month);
-    var dayNum = getDaysofMonth(year, month);
-    var prevMonth = getPrevMonth(year, month);
-
-    var i;
-    var cnt = 0;
-    for (i=0;i<firstDay;i++) {
-        var prevDay = getDaysofMonth(year,prevMonth)-firstDay+i+1;
-        days.push({
-            Day : prevDay.toString(),
-            info : "Nothing",
-            stat : "prev"
-        });
-        cnt++;
-    }
-
-    for (i=1;i<=dayNum;i++) {
-        days.push({
-            Day : i.toString(),
-            info : "Nothing",
-            stat : "curr"
-        })
-        cnt++;
-    }
-
-    var leftDay = 42 - cnt  ;
-    for (i=1;i<=leftDay;i++) {
-        days.push({
-            Day : i.toString(),
-            info : "Nothing",
-            stat : "next"
-        })
-    }
-
-    return days;
-}
-
-const day  = {
-    width : '14%',
-    height : "70px",
-    /*backgroundColor: "lightgray",*/
-    textAlign : "center",
-    marginTop : '1px',
-    fontSize : "20pt",
-    float : 'left',
-    lineHeight : "70px"
-}
-
-const notCurrday  = {
-    width : '14%',
-    height : "70px",
-    /*backgroundColor: "lightgray",*/
-    marginTop : '1px',
-    textAlign : "center",
-    fontSize : "20pt",
-    float : 'left',
-    lineHeight : "70px",
-    color : "#C2C2C2"
 }
 
 const dayweek = ['Sun', 'Mon', 'Tue', 'Wed', "Thu", "Fri", "Sat"];
@@ -142,11 +44,12 @@ class Schedule extends Component {
             Post : "No Post"
         };
 
-        this.nextCalendar = this.nextCalendar.bind(this);
-        this.prevCalendar = this.prevCalendar.bind(this);
+        this.increaseMonth = this.increaseMonth.bind(this);
+        this.decreaseMonth = this.decreaseMonth.bind(this);
+        this.setPost = this.setPost.bind(this);
     }
 
-    prevCalendar () {
+    decreaseMonth () {
         var prevMonth = this.state.Month;
         var prevYear = this.state.Year;
         if (this.state.Month == 1)
@@ -159,7 +62,7 @@ class Schedule extends Component {
         });
     }
 
-    nextCalendar () {
+    increaseMonth () {
         var nextMonth = this.state.Month;
         var nextYear = this.state.Year;
         if (this.state.Month == 12)
@@ -179,46 +82,18 @@ class Schedule extends Component {
     }
 
     render () {
-        var dayList = getDayList(this.state.Year, this.state.Month);
-        var Calendar = dayList.map((item)=> {
-                return (
-                    <div>
-                        {item.stat=="curr"?
-                            <div style={day} onClick={this.setPost.bind(this, item.info)}>
-                                {item.Day}
-                            </div> :
-                            <div style={notCurrday}>
-                                {item.Day}
-                            </div>
-                        }
-                        <div style={{display : 'none'}}>
-                            {item.info}
-                        </div>
-                    </div>
-                );
-            }
-        )
-
         return (
             <div className = "section">
                 <div className = "row" style = {{marginTop : '40px'}}>
                     <div className = "col span-8-of-12">
-                        <div style = {{width : '100%', fontSize :'14px', textAlign : 'center', marginBottom : '5px'}}>
-                            {this.state.Year}
-                        </div>
-                        <div className = "row" style = {{marginBottom : '30px'}}>
-                            <div style = {{float:'left'}} onClick={this.prevCalendar}>
-                                <img src={left} style={{width : '15px', marginTop :'5px'}}/>
-                            </div>
-                            <div style = {{float : "right"}} onClick={this.nextCalendar}>
-                                <img src={right} style={{width : '15px', marginTop :'5px'}}/>
-                            </div>
-                            <div className = "month">
-                                {this.state.Month}
-                            </div>
-                        </div>
+                        <CalendarHead Month={this.state.Month}
+                                      Year={this.state.Year}
+                                      increaseMonth={this.increaseMonth}
+                                      decreaseMonth={this.decreaseMonth}/>
                         {dayofweek}
-                        {Calendar}
+                        <Calendar Month={this.state.Month}
+                                  Year={this.state.Year}
+                                  setPost={this.setPost}/>
 
                     </div>
                     <img src='https://goo.gl/CTk1PE' className = "bar"/>
