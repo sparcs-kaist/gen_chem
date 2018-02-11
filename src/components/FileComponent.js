1431f77ba06492dee41d22468b55b09d62848952
jaejun1 <ltq3021@gmail.com>
2018-02-12 01:35:11 +0900

    add icons for many types on download

--- src/components/FileComponent.js
+++ src/components/FileComponent.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import './FileComponent.css'
-import pdfIcon from './pdfIcon.png'
+import pdfIcon from './img/pdfIcon.png'
+import docIcon from './img/docIcon.png'
+import gifIcon from './img/gifIcon.png'
+import jpgIcon from './img/jpgIcon.png'
+import pngIcon from './img/pngIcon.png'
+import txtIcon from './img/txtIcon.png'
+import xlsIcon from './img/xlsIcon.png'
+import aviIcon from './img/aviIcon.png'
+import movIcon from './img/movIcon.png'
+import anotherIcon from './img/anotherIcon.png'
+
+function getIcon (type){
+    let icons = {'pdf' : pdfIcon, 'doc' : docIcon, 'gif' : gifIcon,
+                'jpg' : jpgIcon, 'png' : pngIcon, 'txt' : txtIcon,
+                'xls' : xlsIcon, 'avi' : aviIcon, 'mov' : movIcon,
+                'docx' : docIcon, 'xlsx' : xlsIcon};
+
+    if (icons.hasOwnProperty (type))
+        return icons [type];
+    else
+        return anotherIcon;
+}
 
 class FileComponent extends Component {
     constructor(props){
@@ -14,7 +35,7 @@ class FileComponent extends Component {
             return (
                 <div className="file">
                     <a href = {this.url}>
-                        <img className="fileIcon" src={pdfIcon}/>
+                        <img className="fileIcon" src={getIcon (this.type)}/>
                         <p className="fileName">{this.name}</p>
                     </a>
                 </div>
